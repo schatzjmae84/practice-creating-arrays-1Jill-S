fee2ae9eaf202fe98f4e8bd1c30c00b71d104f43
Jill Schatz <schatzjm84@gmail.com>
2025-03-19 13:29:42 -0500

    Building Arrays - Lesson 1 Practice: Creating Arrays

---
+++ practice-creating-arrays-1Jill-S.js
@@ -0,0 +1,32 @@
+/*Instructions
+Create a program to practice building and logging arrays.
+
+Tasks
+Task 1: Create Arrays
+Create three separate arrays using array literals for the following:
+● An array of 5 grocery store items.
+● An array of 3 numbers representing prices.
+
+Task 2: Access and Log the Values
+Use console.log() to log the arrays.
+1. Log the entire array of grocery items.
+2. Log the entire array of prices.
+3. Log three individual items of your choice.
+4. Log two prices of your choice.
+*/
+
+let groceryList = ["tortilla chips", "tomatoes", "avocados", "jalepenos", "onions"];
+let prices = [3.59, 1.25, 2.50];
+let sportsTeams = ["Blues", "Cardinals", "Chiefs"];
+
+console.log(groceryList);
+console.log(prices);
+console.log(sportsTeams[1]);
+console.log(groceryList[0]);
+console.log(groceryList[3]);
+console.log(prices[0]);
+console.log(prices[2]);
+
+
+
+
